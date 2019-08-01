1be2a828bc6cd033061760dd47765a4ba03b37af
stefian <stefan.ianta@w3ai.net>
2019-08-01 18:53:45 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+            return {
+
+            }
+        },
+  name: "app"
+}
+</script>
+
+<style>
+
+</style>
