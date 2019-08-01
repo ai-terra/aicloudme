9152a85e2cb8c359cb4dfe99fe9739789cc640d9
stefian <stefan.ianta@w3ai.net>
2019-08-01 19:09:05 -0300

    added Alert button and div

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary">Show Alert</button> 
+        <br><br>
+        <div class="alert alert-info">This is some info</div>
       </div>
     </div>
   </div>
