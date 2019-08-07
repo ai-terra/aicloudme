3c42ec0245c985d39a1373f2e30a05588c95841f
stefian <stefan.ianta@w3ai.net>
2019-08-07 05:08:43 -0300

    wider grid also for sm screens: col-sm-10 col-sm-offset-1

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-10 col-md-offset-1">
+        <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1">
           <h1>ai mass progress</h1>
           <hr />
           <button class="btn btn-primary" @click="show = !show">Job Deal</button>
