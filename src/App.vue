03705a3b7b96080a11cf8655b88dee340ae26d49
stefian <stefan.ianta@w3ai.net>
2019-08-07 05:06:36 -0300

    wider grid to allow for 3 tables: col-md-10 col-md-offset-1

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-10 col-md-offset-1">
           <h1>ai mass progress</h1>
           <hr />
           <button class="btn btn-primary" @click="show = !show">Job Deal</button>
@@ -18,22 +18,10 @@
               <div class="project alert alert-info" v-show="show">
                 <table>
                   <tr>
-                    <th></th>
-                    <th>A</th>
-                    <th>B</th>
-                    <th>C</th>
-                    <th>D</th>
-                    <th>E</th>
-                    <th>F</th>
+                    <th></th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th><th>F</th>
                   </tr>
                   <tr>
-                    <td class="col0">1</td>
-                    <td>Job</td>
-                    <td>status</td>
-                    <td>...</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td class="col0">1</td><td>Job</td><td>status</td><td>...</td><td></td><td></td><td></td>
                   </tr>
                   <tr>
                     <td class="col0-data">a</td>
@@ -675,12 +663,12 @@ td {
 }
 
 .project {
-  width: 49%;
+  width: 32%;
   display: inline-block;
 }
 
 .service {
-  width: 49%;
+  width: 32%;
   display: inline-block;
 }
 
