477e08b8ad2950acb1f63586add4a800680a7a2f
stefian <stefan.ianta@w3ai.net>
2019-09-11 04:57:32 -0400

    subtitle:ai speed innovation

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
             <!-- <div><img id="logo" src="./assets/amp-logo.png"></div> -->
             <div contenteditable="true">
                 <h1 class="title-long">AI PMO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1><h1 class="title-short"> ai cell&nbsp;</h1>
-                <h5>ai team web engine</h5>
+                <h5>ai speed innovation</h5>
             </div>
             <div class="title"><button class="player btn btn-success" @click="pause ? start() : stop()">{{ pause ? '>' : '| |'}}</button></div>
             <div class="title"><button class="player btn btn-warning" @click="play">+</button></div>
