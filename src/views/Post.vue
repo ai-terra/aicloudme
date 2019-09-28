dc06c989868d246b9df296b7bb7d8b2455e9ec9e
stefian <v_ianta@yahoo.com>
2019-09-28 15:35:22 -0400

    Post.vue template and styling

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <!-- <h1>Test Post Component</h1> -->
-    <div v-html="matrix[3]"></div>
-    <!-- <input type="text" v-model="matrix[3]" /> -->
-    <textarea v-model="matrix[3]" placeholder="edit post"></textarea>
+    <div class="title">
+      <h2 v-html="matrix[3]"></h2>
+    </div>
+    <ul class="controls">
+      <li class="react">Like</li>
+      <li class="react">Comment</li>
+      <li class="react">Share</li>
+      <li class="react">Edit</li>
+    </ul>
+    <textarea class="title" v-model="matrix[3]" placeholder="edit post"></textarea>
   </div>
 </template>
 
@@ -14,4 +20,28 @@ export default {
 </script>
 
 <style scoped>
+h2 {
+  margin: 1rem;
+}
+
+.controls {
+  width: 95%;
+  margin-left: 1rem;
+  display: flex;
+  justify-items: center;
+  list-style-type: none;
+  padding: 0;
+}
+
+.react {
+}
+
+.title {
+  margin: 1rem;
+  width: 95%;
+}
+
+textarea.title {
+  background: transparent;
+}
 </style>
