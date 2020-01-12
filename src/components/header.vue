e38728abb4fdc60e69e7d510559fe26a38debf44
stefian <v_ianta@yahoo.com>
2020-01-12 14:48:42 -0400

    changed html and header title to: ai workx for students

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <div class="logo">
-      <router-link to="/">ai workx</router-link>
+      <router-link to="/">ai workx for students</router-link>
     </div>
     <nav>
       <ul>
