ab06206b61498f1db6ad48b6fdff7ba45c26264b
stefian <v_ianta@yahoo.com>
2020-01-18 14:49:03 -0400

    changed Tabs to Spreadshhet view component

---
+++ src/views/Spreadsheet.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="spreadsheet">
+    <Table :matrix="sheet" :tabs-direction="tabs-direction"></Table>
+  </div>
+</template>
+
+<script>
+import Table from "./Table";
+
+export default {
+  name: "Spreadsheet",
+  components: { Table },
+  props: ["sheet", "tabs-direction"]
+};
+</script>
+
+<style scoped>
+</style>
