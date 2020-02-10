404139f2f4a733a1a778b9be38bc9f8716c67ece
stefian <v_ianta@yahoo.com>
2020-02-09 22:26:21 -0400

    changed title to ai colab

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <div class="logo">
-      <router-link to="/">tab colab</router-link>
+      <router-link to="/">ai colab</router-link>
     </div>
     <nav>
       <ul>
