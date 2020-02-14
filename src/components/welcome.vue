5469458f36fc36f7db4c1102d81b799f681efd4b
stefian <v_ianta@yahoo.com>
2020-02-14 07:57:48 -0400

    free, realtime, in mem. tensorflow social engine

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="welcome">
     <h1>colab @ ai speed</h1>
-    <p>free, realtime, in memory colab engine PoC</p>
+    <p>free, realtime, in mem. tensorflow social engine</p>
     <!-- <p>evolutionary learning & fair monetary engine</p> -->
     <div class="cta">
       <router-link to="/signup">Sign Up</router-link>
