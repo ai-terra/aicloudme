468e325e56521c9c4f3a00ad07b2d6a4cd50fa36
stefian <v_ianta@yahoo.com>
2020-03-08 20:55:33 -0300

    added basic vars for the function from event/object

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,11 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.onFileChange = functions.storage.object().onFinalize(event => {
-  console.log(event);
+  // console.log(event);
+  const object = event.metadata;
+  const bucket = object.bucket;
+  const contentType = object.contentType;
+  const filePath = object.name;
+  console.log('File uploaded, function execution started');
   return;
 });
