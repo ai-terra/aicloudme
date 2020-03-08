b2308d4db261cf08de8c2d25036c3b1cb5a2bbd4
stefian <v_ianta@yahoo.com>
2020-03-08 20:31:49 -0300

    changed onChange (deprecated) to onFinalize for onFileChange

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.onFileChange = functions.storage.object().onChange(event => {
+exports.onFileChange = functions.storage.object().onFinalize(event => {
   console.log(event);
   return;
 });
