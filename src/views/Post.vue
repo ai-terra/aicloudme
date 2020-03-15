997f8f63c85154a253b3948615597753dd845599
stefian <v_ianta@yahoo.com>
2020-03-15 17:42:54 -0300

    changed post to single image backgroud

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -4,7 +4,7 @@
     <div class="card" v-for="(post,i) in matrix" :key="i">
       <div
         class="poster"
-        :style="{ backgroundImage: 'url(' + require('./../assets/img/cloud/' + randomInt(100,199) + '.png') + ')', 
+        :style="{ backgroundImage: 'url(' + require('./../assets/img/cloud/' + '1' + '50.png') + ')', 
                   backgroundColor: 'rgba(' 
                     + randomInt(10,250) + ','
                     + randomInt(10,250) + ',' 
