fb3ce9345178013a1129ede4187720d1fe09b676
stefian <v_ianta@yahoo.com>
2020-03-15 19:17:14 -0300

    contribute @ ai speed

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="welcome">
-    <h1 contenteditable="true">ecosystem @ ai speed</h1>
-    <p>fair realtime social and monetary system</p>
+    <h1 contenteditable="true">contribute @ ai speed</h1>
+    <p>fair realtime distributed social and monetary system</p>
     <!-- <p>evolutionary learning & fair monetary engine</p> -->
     <div class="cta">
       <router-link to="/signup">Sign Up</router-link>
