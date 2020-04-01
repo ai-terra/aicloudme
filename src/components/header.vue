e268617c5d11d6847b30df7acf7fa0b120d2196a
stefian <v_ianta@yahoo.com>
2020-04-01 20:47:04 -0300

    changed title to project lead

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <div class="logo">
-      <router-link to="/">projects</router-link>
+      <router-link to="/">project</router-link>
     </div>
     <nav>
       <ul>
