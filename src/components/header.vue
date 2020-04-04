379bf3a8983ca60992ce4597ec04edf015c5da81
stefian <v_ianta@yahoo.com>
2020-04-04 15:00:30 -0300

    changed header to we help each other

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <div class="logo">
-      <router-link to="/">We Help</router-link>
+      <router-link to="/">we help each other</router-link>
     </div>
     <nav>
       <ul>
