f3d32a3ce723e8e1402ae151e3e798845095ffeb
stefian <v_ianta@yahoo.com>
2020-04-08 00:27:02 -0300

    fair realtime distributed business & monetary network

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="welcome">
     <h1 contenteditable="true">business @ ai speed</h1>
-    <p>fair distributed business & monetary network</p>
+    <p>fair realtime distributed business & monetary network</p>
     <!-- <p>evolutionary learning & fair monetary engine</p> -->
     <div class="cta">
       <router-link to="/signup">Sign Up</router-link>
